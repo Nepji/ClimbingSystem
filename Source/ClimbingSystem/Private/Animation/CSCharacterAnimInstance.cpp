de93643a225c93813159b952122454caa59ccb3f
Nepji <114947183+Nepji@users.noreply.github.com>
2025-03-17 21:17:37 +0200

    ++Character Animation : Movement

---
+++ Source/ClimbingSystem/Private/Animation/CSCharacterAnimInstance.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Animation/CSCharacterAnimInstance.h"
+
+#include "CSCharacter.h"
+#include "Components/CSMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+void UCSCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ClimbingSystemCharacter = Cast<ACSCharacter>(TryGetPawnOwner());
+
+	if (ClimbingSystemCharacter)
+	{
+		ClimbingMovementComponent = ClimbingSystemCharacter->GetCustomMovementComponent();
+	}
+}
+void UCSCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!ClimbingSystemCharacter || !ClimbingMovementComponent)
+	{
+		return;
+	}
+
+	GetGroundSpeed();
+	GetAirSpeed();
+	GetShouldMove();
+	GetIsFalling();
+}
+void UCSCharacterAnimInstance::GetGroundSpeed()
+{
+	GroundSpeed = UKismetMathLibrary::VSizeXY(ClimbingSystemCharacter->GetVelocity());
+}
+void UCSCharacterAnimInstance::GetAirSpeed()
+{
+	AirSpeed = ClimbingSystemCharacter->GetVelocity().Z;
+}
+void UCSCharacterAnimInstance::GetShouldMove()
+{
+	const float MinGroundSpeed = 5.f;
+	bShouldMove = ClimbingMovementComponent->GetCurrentAcceleration().Size() > 0 && GroundSpeed > MinGroundSpeed && !bIsFalling;
+}
+void UCSCharacterAnimInstance::GetIsFalling()
+{
+	bIsFalling = ClimbingMovementComponent->IsFalling();
+}
---
+++ Source/ClimbingSystem/Public/Animation/CSCharacterAnimInstance.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CSCharacterAnimInstance.generated.h"
+
+class UCSMovementComponent;
+class ACSCharacter;
+
+
+UCLASS()
+class CLIMBINGSYSTEM_API UCSCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+
+protected:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Reference, meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Reference, meta = (AllowPrivateAccess = "true"))
+	float AirSpeed;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Reference, meta = (AllowPrivateAccess = "true"))
+	bool bShouldMove;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Reference, meta = (AllowPrivateAccess = "true"))
+	bool bIsFalling;
+
+private:
+	UPROPERTY()
+	ACSCharacter* ClimbingSystemCharacter;
+
+	UPROPERTY()
+	UCSMovementComponent* ClimbingMovementComponent;
+
+private:
+	void GetGroundSpeed();
+	void GetAirSpeed();
+	void GetShouldMove();
+	void GetIsFalling();
+	
+};
